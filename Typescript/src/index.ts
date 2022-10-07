b4ca2bac9fb6f85fa5bf683b53199bffa8b7117c
Ronan Klyne <ronan.klyne@cazoo.co.uk>
2022-10-07 17:28:40 +0100

    green

--- Typescript/src/index.ts
+++ Typescript/src/index.ts
@@ -30,80 +30,78 @@ type OrderWork = {
   today: Date;
 };
 
-function newOrder(today: Date, order: Order): OrderWork {
-  const work = {
-    decoratingDays: 0,
-    bakingDays: 0,
-    today,
-    boxingDays: 0,
-    nutsDays: 0,
-  };
-  work.bakingDays += {
-    small: 2,
-    big: 3,
-  }[order.size];
-  if (order.morning) {
-    work.bakingDays -= 1;
-  }
-  if (order.giftWrap) {
-    work.boxingDays = 3;
-  }
-  if (order.frosting) {
-    work.decoratingDays += 2;
-  }
-  if (order.nuts) {
-    work.nutsDays = 1;
-  }
-  return work;
-}
-
 export function orderDue(order: Order): DateString {
-  const work: OrderWork = newOrder(new Date(order.placed), order);
+  const work = new BakingProgress(new Date(order.placed), order);
 
-  doWork(work);
+  const delivery = work.doWork();
 
-  return formatDate(work.today);
+  return formatDate(delivery);
 }
 
-function subtractOneDayWork(work: OrderWork) {
-  const MarcoWorkDays = [1, 2, 3, 4, 5];
-  const SandroWorkDays = [2, 3, 4, 5, 6];
-
-  work.today = addDays(work.today, 1);
-  const marcoIsWorking = MarcoWorkDays.includes(work.today.getDay())
-  const sandroIsWorking = SandroWorkDays.includes(work.today.getDay())
-  if (work.bakingDays) {
-    if (marcoIsWorking) {
-      work.bakingDays -= 1;
+class BakingProgress {
+  private decoratingDays = 0;
+  private bakingDays = 0;
+  private boxingDays = 0;
+  private nutsDays = 0;
+  constructor(private today: Date, order: Order) {
+    this.bakingDays += {
+      small: 2,
+      big: 3,
+    }[order.size];
+    if (order.morning) {
+      this.bakingDays -= 1;
     }
-  } else if (work.decoratingDays) {
-    if (sandroIsWorking) {
-      work.decoratingDays -= 1;
+    if (order.giftWrap) {
+      this.boxingDays = 3;
     }
-  } else if (work.nutsDays) {
-    if (marcoIsWorking) {
-      work.nutsDays -= 1;
+    if (order.frosting) {
+      this.decoratingDays += 2;
+    }
+    if (order.nuts) {
+      this.nutsDays = 1;
     }
   }
 
-  if (work.boxingDays) {
-    work.boxingDays -= 1;
+  subtractOneDayWork() {
+    const MarcoWorkDays = [1, 2, 3, 4, 5];
+    const SandroWorkDays = [2, 3, 4, 5, 6];
+
+    this.today = addDays(this.today, 1);
+    const marcoIsWorking = MarcoWorkDays.includes(this.today.getDay());
+    const sandroIsWorking = SandroWorkDays.includes(this.today.getDay());
+    if (this.bakingDays) {
+      if (marcoIsWorking) {
+        this.bakingDays -= 1;
+      }
+    } else if (this.decoratingDays) {
+      if (sandroIsWorking) {
+        this.decoratingDays -= 1;
+      }
+    } else if (this.nutsDays) {
+      if (marcoIsWorking) {
+        this.nutsDays -= 1;
+      }
+    }
+
+    if (this.boxingDays) {
+      this.boxingDays -= 1;
+    }
   }
-}
 
-function doWork(work: OrderWork) {
-  const workIsDone = () => {
-    return !(
-      work.bakingDays ||
-      work.boxingDays ||
-      work.decoratingDays ||
-      work.nutsDays
-    );
-  };
+  doWork() {
+    const workIsDone = () => {
+      return !(
+        this.bakingDays ||
+        this.boxingDays ||
+        this.decoratingDays ||
+        this.nutsDays
+      );
+    };
 
-  while (!workIsDone()) {
-    subtractOneDayWork(work);
+    while (!workIsDone()) {
+      this.subtractOneDayWork();
+    }
+
+    return this.today;
   }
-
-  return formatDate(work.today);
 }
