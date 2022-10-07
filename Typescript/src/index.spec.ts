da359b43f2d4331a8a073e08c6bcd1a826ea2c3a
Ronan Klyne <ronan.klyne@cazoo.co.uk>
2022-10-07 15:44:44 +0100

    green

--- Typescript/src/index.spec.ts
+++ Typescript/src/index.spec.ts
@@ -6,7 +6,7 @@ describe("one small cake", () => {
   test("ordered on Monday, is delivered on Wednesday", () => {
     const Monday = "2022-10-10";
     const Wednesday = "2022-10-12";
-    const result = deliveryDate(order, Monday);
+    const result = orderDue({ size: order, placed: Monday, morning: false });
     expect(result).toBe(Wednesday);
   });
 
