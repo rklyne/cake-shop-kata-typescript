2821f2f90b263b791dde391ca6e51bda5ed58651
Ronan Klyne <ronan.klyne@cazoo.co.uk>
2022-10-07 15:34:59 +0100

    red

--- Typescript/src/index.spec.ts
+++ Typescript/src/index.spec.ts
@@ -29,8 +29,19 @@ test("marco only bakes Monday - Friday", () => {
 });
 
 test("big cakes take 3 days", () => {
+  const order = "big";
+
   const Monday = "2022-10-10";
   const Thursday = "2022-10-13";
-  const order = "big";
   expect(deliveryDate(order, Monday)).toBe(Thursday);
 });
+
+test("orders in the morning start same day", () => {
+  const Monday = "2022-10-10";
+  const Tuesday = "2022-10-11";
+  const order = {
+    size: "small",
+    placed: Monday,
+    morning: true,
+  };
+  expect(orderDue(order)).toBe(Tuesday);
