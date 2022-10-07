a62da3aaeb4818dcb83822678890f2db31e95bd3
Ronan Klyne <ronan.klyne@cazoo.co.uk>
2022-10-07 15:04:47 +0100

    red

--- Typescript/src/index.spec.ts
+++ Typescript/src/index.spec.ts
@@ -1,8 +1,19 @@
 import { deliveryDate } from ".";
 
-test("A small cake, ordered on Monday, is delivered on Wednesday", () => {
-  const Monday = "2022-10-10";
-  const Wednesday = "2022-10-12";
-  const result = deliveryDate("small", Monday);
-  expect(result).toBe(Wednesday);
+describe("one small cake", () => {
+  const order = "small";
+
+  test("ordered on Monday, is delivered on Wednesday", () => {
+    const Monday = "2022-10-10";
+    const Wednesday = "2022-10-12";
+    const result = deliveryDate(order, Monday);
+    expect(result).toBe(Wednesday);
+  });
+
+  test("ordered on Tuesday, is delivered on Thursday", () => {
+    const Tuesday = "2022-10-11";
+    const Thursday = "2022-10-13";
+    const result = deliveryDate(order, Tuesday);
+    expect(result).toBe(Thursday);
+  });
 });
