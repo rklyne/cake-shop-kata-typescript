67c9a4422f90e4c7018c548440d11e747789241a
Ronan Klyne <ronan.klyne@cazoo.co.uk>
2022-10-07 15:11:00 +0100

    green

--- Typescript/src/index.ts
+++ Typescript/src/index.ts
@@ -1,6 +1,9 @@
 type Size = "small" | "big";
 
 export function deliveryDate(size: Size, date: string) {
-  size; date;
-  return "2022-10-12";
+  size;
+  const day = 60 * 60 * 24 * 1000;
+  const delivery = new Date(date);
+
+  return new Date(delivery.getTime() + 2 * day).toISOString().slice(0, 10);
 }
